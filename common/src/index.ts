5cebfc75089b544d78b6258c2f8e61f181135073
Suryadev78 <pandeysuryadev00@gmail.com>
2024-09-19 13:16:37 +0530

    created common folder for sharing of zod validation

---
+++ common/src/index.ts
@@ -0,0 +1,34 @@
+// This file is used to define the input types for the API that will be used by both the frontend and backend
+
+import z from "zod";
+
+export const signupInput = z.object({
+  name: z.string().min(4),
+  email: z.string().email(),
+  password: z.string().min(4),
+});
+
+export const signInInput = z.object({
+  email: z.string().email(),
+  password: z.string().min(4),
+});
+
+export const blogCreationInput = z.object({
+  title: z.string().min(4),
+  content: z.string().min(15),
+});
+
+export const blogUpdateInput = z.object({
+  title: z.string().min(4),
+  content: z.string().min(15),
+});
+
+export const blogDeleteInput = z.object({
+  id: z.number(),
+});
+
+export type signupInputType = z.infer<typeof signupInput>;
+export type signInputType = z.infer<typeof signInInput>;
+export type blogCreationInputType = z.infer<typeof blogCreationInput>;
+export type blogUpdateInputType = z.infer<typeof blogUpdateInput>;
+export type blogDeleteInputType = z.infer<typeof blogDeleteInput>;
