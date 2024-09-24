305386a2e62a4860c337de191a9359c9eb4cc3d5
Suryadev78 <pandeysuryadev00@gmail.com>
2024-09-25 00:16:46 +0530

    completed frontend/backend

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,4 @@
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
-
-const publicKey = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
-if (!publicKey) {
-  throw new Error("Missing VITE_CLERK_PUBLISHABLE_KEY");
-}
-
 createRoot(document.getElementById("root")!).render(<App />);
